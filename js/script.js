2a3d0012dadc150260ab8154e31ca831ba325f8c
Byron Blocker <byronblocker@gmail.com>
2017-09-01 10:50:51 -0700

    lesson on Jquery fadeIn, fadeOut, and fadeToggle

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+   $(".red-box").fadeTo(2000, 0.25);
+   $(".green-box").fadeTo(1750, 0.5);
+   $(".blue-box").fadeTo(1500, 0.75);
+
+   $(".green-box").fadeToggle();
+    $(".green-box").fadeToggle();
+
 });
